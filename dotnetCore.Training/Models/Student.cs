abe8ad78185aad00ad753404c28b5acc0b2fa5f8
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-07-25 12:45:07 +0530

    dotnetCore sesion 1

---
+++ dotnetCore.Training/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnetCore.Training.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+
+
+    }
+}
