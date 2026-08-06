96782a87f9a52f4e0bec9d549169b5f4e2a4e196
ashishkhadye33 <ashishkhadye33@gmail.com>
2026-08-06 12:39:36 +0530

    dotnetCore session 6

---
+++ LA.Repositories/Interfaces/IDistrictRepo.cs
@@ -0,0 +1,24 @@
+﻿using LA.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA.Repositories.Interfaces
+{
+    public interface IDistrictRepo
+    {
+        //return type, method name(parameters)
+
+        IEnumerable<District> GetAll();
+
+        District GetById(int id);
+
+        void Save(District district);
+
+        void Edit(District district);
+
+        void RemoveData(District district);
+    }
+}
